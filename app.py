500762e5dedaad216d7768d298f41dd9ddc32dec
Contra-BlackFlag <pvvishwa12345@gmail.com>
2026-02-01 11:09:39 +0530

    DataBase Implementation

--- app.py
+++ app.py
@@ -11,12 +11,46 @@ db = SQLAlchemy(app=app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(15), nullable = False)
+    user_name = db.Column(db.String(15), nullable = False, unique = True)
     password = db.Column(db.String(15), nullable = False)
     date_of_login = db.Column(db.DateTime,default = datetime.utcnow)
+    complaints = db.relationship("complaint", backref = "user", lazy = True)
     def __repr__(self)->str:
         return f"{self.user_name}"
-      
+
+class complaint(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    description = db.Column(db.String(200), nullable = False)
+    time_of_creation = db.Column(db.DateTime, default = datetime.date)
+    
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable = False
+    )
+    images = db.relationship("complaintImage", backref = "complaint", lazy = True)
+    location = db.relationship("Location", backref = "complaint", uselist =True)
+
+class location(db.Model):
+    complaint_id = db.Column(
+        db.Integer,
+        db.ForeignKey("complaint.id"),
+        primary_key = True
+    )
+    latitude = db.Column(db.Float, nullable = False)
+    longitute = db.Column(db.Float, nullable = False)
+
+class ComplaintImages(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     image_path = db.Column(db.String(255), nullable=False)
+
+     complaint_id = db.Column(
+        db.Integer,
+        db.ForeignKey("complaint.id"),
+        nullable=False
+     )
+
 
 @app.route("/testing")
 def testing():
